89ba5a69b048f3cc1d9bc414193ad729be2ac06d
vinhabubu <77477174+vinhabubu@users.noreply.github.com>
2022-05-04 17:14:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ function sum (a = 0, b = 0) {
   return a + b
 }
 
-function sub(a=0, b=0) {
-  return a - b
+function sub(a=0, b=0, c=0) {
+  return a - b*c
 }
 
 const total = sum(10, 1)
-console.log(sub(9, 1))
+console.log(sub(9, 1, 2))
 console.log('total', total)
