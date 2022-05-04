59364da05912222fd78ded1bbe8a4d28926f301d
Van Quang <vanquang.3799@gmail.com>
2022-05-04 14:31:29 +0700

    Test git

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-function sum (a = 0, b = 0) {
-  return a + b
+function sum(a = 0, b = 0) {
+  return a + b;
 }
 
-const total = sum(10, 1)
-console.log('total', total)
+const total = sum(10, 1);
+console.log("total", total);
+
+console.log("Test Git");
