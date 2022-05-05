ae59182388804ed99ee1b0e3355cfd2739947697
VQuang <>
2022-05-05 23:44:57 +0700

    Update calculator

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ function sum(a = 0, b = 0) {
   return a + b;
 }
 
-const total = sum(10, 1);
-console.log("total", total);
+function subtract(a = 0, b = 0) {
+  return a - b;
+}
 
-console.log("Test Git");
+function multi(a = 0, b = 1) {
+  return a * b;
+}
+
+function divi(a = 0, b = 1) {
+  return a / b;
+}
+
+const totalSum = sum(10, 1);
+const totalSubtract = subtract(10, 1);
+const totalMulti = multi(10, 2);
+const totalDivi = divi(10, 5);
+
+console.log("total sum: ", totalSum);
+console.log("total subtract: ", totalSubtract);
+console.log("total multi: ", totalMulti);
+console.log("total divi: ", totalDivi);
